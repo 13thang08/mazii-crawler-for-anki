7e8201b550f8ce9348830df5bb99ce36aa48e657
Vu Manh Thang <thang@zyyx.jp>
2019-06-27 08:58:50 +0700

    get all data

--- app.js
+++ app.js
@@ -12,17 +12,99 @@ const { throwError, from, of, Observable } = require('rxjs');
 const { catchError, retry, tap, retryWhen, delay, take } = require('rxjs/operators');
 const get = require('get-value');
 
-async function appendCsvFile(asins) {
-  let fields = ["ASIN"];
+const input_file = "kanji_list.csv";
+const output_file = "output.csv";
+
+const fields = [
+  'kanji',
+  'mean',
+  'kun',
+  'on',
+  'stroke_count',
+  'level',
+  'freq',
+  'compDetail1_w',
+  'compDetail1_h',
+  'compDetail2_w',
+  'compDetail2_h',
+  'compDetail3_w',
+  'compDetail3_h',
+  'compDetail4_w',
+  'compDetail4_h',
+  'detail1',
+  'detail2',
+  'detail3',
+  'detail4',
+  'detail5',
+  'suggestions1',
+  'suggestions2',
+  'suggestions3',
+  'suggestions4',
+  'examples1_w',
+  'examples1_p',
+  'examples1_m',
+  'examples1_h',
+  'examples2_w',
+  'examples2_p',
+  'examples2_m',
+  'examples2_h',
+  'examples3_w',
+  'examples3_p',
+  'examples3_m',
+  'examples3_h',
+  'examples4_w',
+  'examples4_p',
+  'examples4_m',
+  'examples4_h',
+  'examples5_w',
+  'examples5_p',
+  'examples5_m',
+  'examples5_h',
+  'examples6_w',
+  'examples6_p',
+  'examples6_m',
+  'examples6_h',
+  'examples7_w',
+  'examples7_p',
+  'examples7_m',
+  'examples7_h',
+  'examples8_w',
+  'examples8_p',
+  'examples8_m',
+  'examples8_h',
+  'examples9_w',
+  'examples9_p',
+  'examples9_m',
+  'examples9_h',
+  'examples10_w',
+  'examples10_p',
+  'examples10_m',
+  'examples10_h',
+];
+
+async function appendCsvFile(data) {
   const json2csvParser = new Json2csvParser({ fields, header: false });
-  const csv = json2csvParser.parse(asins);
-  await appendFile(config.productsFile, `${endOfLine}`);
-  await appendFile(config.productsFile, csv);
+  const csv = json2csvParser.parse([data]);
+  await appendFile(output_file, `${endOfLine}`);
+  await appendFile(output_file, csv);
 }
 
 async function main() {
-  let row = await getCsvRow('勝');
-  console.log(row);
+  // clean up
+  try {
+    await unlink(output_file);
+  } catch (error) {
+    
+  }
+
+  let inputs = await csv2json().fromFile(input_file);
+  let kanji_list = inputs.map(x => x.kanji);
+  for (let i = 0; i < kanji_list.length; i++) {
+    let kanji = kanji_list[i];
+    let row = await getCsvRow(kanji);
+    await appendCsvFile(row);
+    console.log(`progress ${i+1}/${kanji_list.length}`);
+  }
 
   process.exit();
 }
@@ -105,8 +187,8 @@ async function getCsvRow(word) {
   let data = {};
   data['kanji'] = result.kanji;
   data['mean'] = result.mean;
-  data['on'] = result.on;
   data['kun'] = result.kun;
+  data['on'] = result.on;
   data['stroke_count'] = result.stroke_count;
   data['level'] = result.level;
   data['freq'] = result.freq;
@@ -119,6 +201,22 @@ async function getCsvRow(word) {
   data['compDetail3_h'] = get(compDetail, '2.h');
   data['compDetail4_w'] = get(compDetail, '3.w');
   data['compDetail4_h'] = get(compDetail, '3.h');
+
+  let detailArr;
+  if (result.detail) {
+    detailArr = result.detail.split("##");
+  }
+  data['detail1'] = get(detailArr, '0');
+  data['detail2'] = get(detailArr, '1');
+  data['detail3'] = get(detailArr, '2');
+  data['detail4'] = get(detailArr, '3');
+  data['detail5'] = get(detailArr, '4');
+
+  data['suggestions1'] = get(mean, 'result.0.mean');
+  data['suggestions2'] = get(mean, 'result.1.mean');
+  data['suggestions3'] = get(mean, 'result.2.mean');
+  data['suggestions4'] = get(mean, 'result.3.mean');
+
   let examples = result.examples;
   data['examples1_w'] = get(examples, '0.w');
   data['examples1_p'] = get(examples, '0.p');
@@ -160,11 +258,6 @@ async function getCsvRow(word) {
   data['examples10_p'] = get(examples, '9.p');
   data['examples10_m'] = get(examples, '9.m');
   data['examples10_h'] = get(examples, '9.h');
-
-  data['suggestions1'] = get(mean, 'result.0.mean')
-  data['suggestions2'] = get(mean, 'result.1.mean')
-  data['suggestions3'] = get(mean, 'result.2.mean')
-  data['suggestions4'] = get(mean, 'result.3.mean')
 
   return data;
 }
